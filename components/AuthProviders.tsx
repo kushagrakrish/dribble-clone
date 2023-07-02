0ebd55a54e51eb425af1060e6666bd9846813c4c
@kushagra24 <krishna619kk@gmail.com>
2023-07-02 16:11:09 +0530

    Next Auth setup ,facing bug will fix it

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import React, { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+};
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      console.log(res);
+      setProviders(res as any);
+    };
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object?.values(providers)?.map((provider: Provider, i) => (
+          <button key={i}>{provider.id}</button>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AuthProviders;
