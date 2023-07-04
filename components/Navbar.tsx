2b465b2ace897f7fc86d160a5a170dff472b98f9
@kushagra24 <krishna619kk@gmail.com>
2023-07-04 14:31:06 +0530

    Profile implemented in navbar by replacing code with ProfileMneu component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,8 @@ import logo from "@/public/logo.svg";
 import { NavLinks } from "@/constants";
 import AuthProviders from "./AuthProviders";
 import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -26,15 +28,7 @@ const Navbar = async () => {
         <div className='flexCenter gap-4'>
           {session?.user ? (
             <>
-              {session?.user?.image && (
-                <Image
-                  src={session?.user?.image}
-                  width={40}
-                  height={40}
-                  className='rounded-full'
-                  alt='user'
-                />
-              )}
+              {session?.user?.image && <ProfileMenu session={session} />}
               <Link href={"/create-project"}>Share Work</Link>
             </>
           ) : (
